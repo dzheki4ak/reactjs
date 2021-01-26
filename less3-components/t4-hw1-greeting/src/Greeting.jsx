dfbd677f8c9deaebb49d0ccb79e1bf2076f12711
ki4a <Igor.ki4@gmail.com>
2021-01-26 11:17:42 +0200

    4

--- less3-components/t4-hw1-greeting/src/Greeting.jsx
+++ less3-components/t4-hw1-greeting/src/Greeting.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './index.scss';
 
 const yearInMs = 1000 * 60 * 60 * 24 * 365;
-const Greeting = (props) => {
-    return <div className="greeting">
+const curentDate = new Date('2018-01-01T11:11:11.819Z')
+const Greeting = props => {
+  return (
+    <div className="greeting">
       {`My name is ${props.firstName} is ${props.lastName}.
-      I'm ${Math.floor((new Date() - props.birthDate) / yearInMs)} years old`}
-      </div>
-}
+      I'm ${Math.floor((curentDate - props.birthDate) / yearInMs)} years old`}
+    </div>
+  );
+};
 
 export default Greeting;
--- less3-components/t4-hw1-greeting/src/index.jsx
+++ less3-components/t4-hw1-greeting/src/index.jsx
@@ -4,5 +4,7 @@ import Greeting from './Greeting';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Greeting firstName="John" lastName="Doe"
-birthDate={new Date('2001-01-01T11:11:11.819Z')} />, rootElement);
+ReactDOM.render(
+  <Greeting firstName="John" lastName="Doe" birthDate={new Date('2001-01-01T11:11:11.819Z')} />,
+  rootElement,
+);
