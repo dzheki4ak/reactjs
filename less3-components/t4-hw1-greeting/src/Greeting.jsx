912a3372c83b62ea68f1b63290414e55a4cb3d70
ki4a <Igor.ki4@gmail.com>
2021-01-26 10:58:45 +0200

    4

--- less3-components/t4-hw1-greeting/src/Greeting.jsx
+++ less3-components/t4-hw1-greeting/src/Greeting.jsx
@@ -3,7 +3,7 @@ import './index.scss';
 
 const Greeting = (props) => {
     return <div className="greeting">
-      {`My ${props.firstName} is ${props.lastName}. I'm ${props.age} years old`}
+      {`My name is ${props.firstName} is ${props.lastName}. I'm ${props.age} years old`}
       </div>
 }
 
