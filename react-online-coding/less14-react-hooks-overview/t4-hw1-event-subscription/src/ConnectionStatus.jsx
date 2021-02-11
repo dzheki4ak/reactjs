3400f0f4a5157a477deb45c40ef1df9baa358589
ki4a <Igor.ki4@gmail.com>
2021-02-11 17:04:53 +0200

    14 - 4

---
+++ react-online-coding/less14-react-hooks-overview/t4-hw1-event-subscription/src/ConnectionStatus.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+const ConnectionStatus = () => {
+  const [status, setSatus] = useState(true);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setSatus(true);
+    };
+
+    const handleOffline = () => {
+      setSatus(false);
+    };
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  return (
+    <div className={status ? 'status' : 'status status_offline'}>
+      {status ? 'Online' : 'Offline'}
+    </div>
+  );
+};
+
+export default ConnectionStatus;
